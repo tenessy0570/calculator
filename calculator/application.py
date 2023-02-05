52027eeb9a8e3544b4321f8620db50c33ca62d17
tenessy0570 <tenessy0570@gmail.com>
2023-02-05 09:13:27 +0700

    created prompt. created validator for it

--- calculator/application.py
+++ calculator/application.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLineEdit
+
+from calculator.validators import PromptValidator
 
 
 class Calculator:
@@ -10,25 +12,50 @@ class Calculator:
         self.main_window.setFixedHeight(602)
         self.main_window.show()
 
-        self.numbers_window = None
-        self.numbers_window_buttons = None
+        self.numbers_grid: QWidget = None
+        self.numbers_grid_buttons = None
+        self.prompt_window: QLineEdit = None
 
         self.create_buttons_grid()
+        self.create_prompt_window()
 
-    def tests_method(self):
-        print("Clicked!")
+    def handle_number_grid_click(self, checked: bool):
+        sender: QPushButton = self.numbers_grid.sender()
+        value = sender.text()
+        self.prompt_window.insert(value)
+
+    def create_prompt_window(self):
+        self.prompt_window = QLineEdit(self.main_window)
+        self.prompt_window.show()
+        self.prompt_window.setValidator(PromptValidator)
+        numbers_grid_geometry = self.numbers_grid.geometry()
+
+        prompt_width = numbers_grid_geometry.width()
+        prompt_height = 65
+
+        prompt_pos_x = numbers_grid_geometry.x()
+        prompt_pos_y = numbers_grid_geometry.y() - 5 - prompt_height
+        self.prompt_window.setGeometry(prompt_pos_x, prompt_pos_y, prompt_width, prompt_height)
+        self.prompt_window.setStyleSheet("""
+            border: 1px solid black;
+            padding: 0px 10px;
+            border-radius: 5%;
+            font-size: 20px
+        """)
+        self.prompt_window.setMaxLength(8)
 
     def create_buttons_grid(self):
-        self.numbers_window = QWidget(self.main_window)
-        self.numbers_window.show()
+        self.numbers_grid = QWidget(self.main_window)
+        self.numbers_grid.show()
 
         button_width = 120
         button_height = 75
         grid_pos_x = 150
         grid_pos_y = 200
-        self.numbers_window.setGeometry(grid_pos_x, grid_pos_y, button_width * 3, button_height * 3)
+        self.numbers_grid.setGeometry(grid_pos_x, grid_pos_y, button_width * 3, button_height * 4)
+        self.numbers_grid.setStyleSheet("font-size: 18px")
 
-        self.numbers_window_buttons = [
+        self.numbers_grid_buttons = [
             [
                 QPushButton("1"),
                 QPushButton("2"),
@@ -44,15 +71,23 @@ class Calculator:
                 QPushButton("8"),
                 QPushButton("9")
             ],
+            QPushButton("0")
         ]
 
-        for row, columns_list in enumerate(self.numbers_window_buttons):
-            for column, button in enumerate(columns_list):
-                button.setParent(self.numbers_window)
-                button.setGeometry(column * button_width, row * button_height, button_width, button_height)
-                button.setCheckable(True)
+        for row, columns_list in enumerate(self.numbers_grid_buttons):
+            if not isinstance(columns_list, list):
+                button = columns_list
+                button.setParent(self.numbers_grid)
+                button.setGeometry(1, 3 * button_height, 3 * button_width, 1 * button_height)
                 button.show()
-                button.clicked.connect(self.tests_method)
+                button.clicked.connect(self.handle_number_grid_click)
+                continue
+
+            for column, button in enumerate(columns_list):
+                button.setParent(self.numbers_grid)
+                button.setGeometry(column * button_width, row * button_height, button_width, button_height)
+                button.show()
+                button.clicked.connect(self.handle_number_grid_click)
 
     def start(self) -> None:
         self.pyqt_app.exec()
